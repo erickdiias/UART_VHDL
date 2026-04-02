2e2d9e6b74e80806f07f8702f9908852994ba4c4
erickdiias__ <soares8erick@gmail.com>
2026-04-02 13:35:26 -0300

    Edit

--- UART.srcs/sources_1/imports/UART/uart_tx.vhd
+++ UART.srcs/sources_1/imports/UART/uart_tx.vhd
@@ -1,3 +1,4 @@
+-- Esse
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -18,7 +19,7 @@ entity uart_tx is
 end uart_tx;
 
 architecture Behavioral of uart_tx is
-    type state_type is (TX_IDLE, TX_START, TX_DATA, TX_PARITY, TX_STOP);
+    type state_type is (TX_IDLE, TX_START_BIT, TX_DATA_BIT, TX_PARITY_BIT, TX_STOP_BIT);
     signal state : state_type := TX_IDLE;
 
     signal bit_count : integer range 0 to 7 := 0;
@@ -43,40 +44,40 @@ begin
                         bit_count <= 0;
 
                         if i_tx_enable = '1' then
-                            shift_reg <= i_tx_data;
                             -- Cálculo da paridade (par por padrão)
                             parity_bit <= i_tx_data(0) xor i_tx_data(1) xor
-                                        i_tx_data(2) xor i_tx_data(3) xor
-                                        i_tx_data(4) xor i_tx_data(5) xor
-                                        i_tx_data(6) xor i_tx_data(7);
-                            
-                            state <= TX_START;
+                                          i_tx_data(2) xor i_tx_data(3) xor
+                                          i_tx_data(4) xor i_tx_data(5) xor
+                                          i_tx_data(6) xor i_tx_data(7);
+
+                            state <= TX_START_BIT;
                         end if;
 
-                    when TX_START =>
+                    when TX_START_BIT =>
+                        shift_reg <= i_tx_data;
                         o_tx <= '0'; -- Start bit
                         o_tx_busy <= '1';
-                        state <= TX_DATA;
+                        state <= TX_DATA_BIT;
                         bit_count <= 0;
 
-                    when TX_DATA =>
+                    when TX_DATA_BIT =>
                         o_tx <= shift_reg(bit_count);
                         bit_count <= bit_count + 1;
 
                         if bit_count = 7 then
-                            state <= TX_PARITY;
+                            state <= TX_PARITY_BIT;
                         end if;
 
-                    when TX_PARITY =>
+                    when TX_PARITY_BIT =>
                         -- Ajusta a paridade conforme o tipo (0 = par, 1 = ímpar)
                         if i_tx_parity = '0' then
                             o_tx <= parity_bit;           -- Paridade par
                         else
                             o_tx <= not parity_bit;       -- Paridade ímpar
                         end if;
-                        state <= TX_STOP;
+                        state <= TX_STOP_BIT;
 
-                    when TX_STOP =>
+                    when TX_STOP_BIT =>
                         o_tx <= '1'; -- Stop bit
                         o_tx_busy <= '1';
                         state <= TX_IDLE;
